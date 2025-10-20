ce8cc5903bb7e2272ded18e60e32fc8749968097
Yan Mendes <cliente@172-0-20-32.lightspeed.iplsin.sbcglobal.net>
2025-10-20 14:01:24 -0300

    Corrigindo alias e excluindo componente

--- src/components/atoms/Button/ButtonType.ts
+++
@@ -1,9 +0,0 @@
-import type { ReactNode, ButtonHTMLAttributes } from "react";
-
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary" | "accent" | "outline" | "ghost";
-  size?: "sm" | "md" | "lg";
-  children: ReactNode;
-  isLoading?: boolean;
-  className?: string;
-}
