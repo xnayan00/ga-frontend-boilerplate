4009b17ac88be414633efd4b3351de7280ff27b3
xnayan00 <yanricardo.js@gmail.com>
2025-11-03 08:13:05 -0300

    Atualizando package-lock

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background">
+    <main className="flex min-h-screen items-center justify-center bg-background">
       <h1>Frontend Boilerplate</h1>
-    </div>
+    </main>
   );
 }
